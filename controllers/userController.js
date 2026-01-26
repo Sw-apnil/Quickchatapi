dd9bc8d371ef247293e9b20da9a0666f656502f8
Swapnil Saurabh <swapnilsaurabh@Swapnils-MacBook-Air.local>
2026-01-27 00:25:08 +0530

    controller to update user profile is added

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,8 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { generateToken } from "../lib/utils.js";
+import cloudinary from "../lib/cloudinary.js";
+
 
 
 
@@ -81,4 +83,37 @@ export const checkAuth = (req, res) => {
     success: true,
     user: req.user,
   });
+};
+
+
+
+//Controller to update user profile
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic, bio, fullName } = req.body;
+    const userId = req.user._id;
+
+    let updateData = { bio, fullName };
+
+    if (profilePic) {
+      const uploadResponse = await cloudinary.uploader.upload(profilePic);
+      updateData.profilePic = uploadResponse.secure_url;
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(userId, updateData, {
+      new: true,
+    });
+
+    res.json({
+      success: true,
+      user: updatedUser,
+      message: "User updated successfully",
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
