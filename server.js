cac66d7df2405bab31431a73abbcc9fb5df431fc
Swapnil Saurabh <swapnilsaurabh@Swapnils-MacBook-Air.local>
2026-01-26 23:01:51 +0530

    database connected and user model made

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import http from "http";
+import {connectDB} from "./lib/db.js";
+
+
+
+
+//Create Express app and HTTP server
+const app = express();
+const server = http.createServer(app);
+
+//Middleware setup
+app.use(express.json({ limit: "4mb" }));
+app.use(cors());
+
+
+// connect to db
+await connectDB();
+
+//Routes setup
+app.use("/api/status", (req, res) => {
+  res.send("Server is live");
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
