844f77dcda3a3e3d80d716d945afec9fce153645
Swapnil Saurabh <swapnilsaurabh@Swapnils-MacBook-Air.local>
2026-01-26 23:44:03 +0530

    controller function made to authenticate the user

--- controllers/userController.js
+++ controllers/userController.js
@@ -73,3 +73,12 @@ export const login = async (req, res) => {
     });
   }
 };
+
+//Contoller to check if user is authenticated
+
+export const checkAuth = (req, res) => {
+  res.json({
+    success: true,
+    user: req.user,
+  });
+};
